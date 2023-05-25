Zondax/ledger-mina

stax: compile for Stax

Makefile
@@ -35,6 +35,8 @@ DEFINES += $(DEFINES_LIB) $(USER_DEFINES)
 
 ifeq ($(TARGET_NAME),TARGET_NANOS)
 	ICONNAME=icons/nanos_app_mina.gif
+else ifeq ($(TARGET_NAME),TARGET_FATSTACKS)
+	ICONNAME=icons/stax_app_mina.gif
 else
 	ICONNAME=icons/nanox_app_mina.gif
 endif
@@ -90,6 +92,8 @@ ifeq ($(TARGET_NAME),TARGET_NANOX)
 EMULATOR_SDK=1.2
 else ifeq ($(TARGET_NAME),TARGET_NANOS2)
 EMULATOR_SDK=1.0
+else ifeq ($(TARGET_NAME),TARGET_FATSTACKS)
+EMULATOR_SDK=1.0
 else
 EMULATOR_SDK=2.0
 endif
@@ -119,6 +123,10 @@ ifeq ($(TARGET_NAME),TARGET_NANOS)
 EMULATOR_MODEL=nanos
 endif
 
+ifeq ($(TARGET_NAME),TARGET_FATSTACKS)
+EMULATOR_MODEL=fat
+endif
+
 ifeq ("$(EMULATOR_MODEL)","")
 $(error Unknown TARGET_NAME '$(TARGET_NAME)')
 endif
@@ -137,7 +145,12 @@ endif
 
 DEFINES   += LEDGER_BUILD
 DEFINES   += OS_IO_SEPROXYHAL
-DEFINES   += HAVE_BAGL HAVE_SPRINTF
+DEFINES   += HAVE_SPRINTF
+ifneq ($(TARGET_NAME),TARGET_FATSTACKS)
+DEFINES += HAVE_BAGL HAVE_UX_FLOW
+else
+DEFINES += NBGL_QRCODE
+endif
 DEFINES   += HAVE_IO_USB HAVE_L4_USBLIB IO_USB_MAX_ENDPOINTS=6 IO_HID_EP_LENGTH=64 HAVE_USB_APDU
 DEFINES   += LEDGER_MAJOR_VERSION=$(APPVERSION_M) LEDGER_MINOR_VERSION=$(APPVERSION_N) LEDGER_PATCH_VERSION=$(APPVERSION_P)
 
@@ -163,7 +176,9 @@ DEFINES       += IO_SEPROXYHAL_BUFFER_SIZE_B=128
 else
 DEFINES       += BUILD_NANOX IO_SEPROXYHAL_BUFFER_SIZE_B=300
 DEFINES       += HAVE_GLO096
-DEFINES       += HAVE_BAGL BAGL_WIDTH=128 BAGL_HEIGHT=64
+ifneq ($(TARGET_NAME), TARGET_FATSTACKS)
+DEFINES       += BAGL_WIDTH=128 BAGL_HEIGHT=64
+endif
 DEFINES       += HAVE_BAGL_ELLIPSIS # long label truncation feature
 DEFINES       += HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX
 DEFINES       += HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX
@@ -257,7 +272,11 @@ include $(BOLOS_SDK)/Makefile.glyphs
 ### variables processed by the common makefile.rules of the SDK to grab source files and include dirs
 APP_SOURCE_PATH  += src
 SDK_SOURCE_PATH  += lib_stusb lib_stusb_impl lib_u2f
-SDK_SOURCE_PATH  += lib_ux
+ifeq ($(TARGET_NAME),TARGET_FATSTACKS)
+SDK_SOURCE_PATH += lib_nbgl/src
+else
+SDK_SOURCE_PATH += lib_ux
+endif
 
 ifeq ($(TARGET_NAME),TARGET_NANOX)
 SDK_SOURCE_PATH  += lib_blewbxx lib_blewbxx_impl

LedgerHQ/ledger-app-mina

Makefile
@@ -35,6 +35,8 @@ DEFINES += $(DEFINES_LIB) $(USER_DEFINES)
 
 ifeq ($(TARGET_NAME),TARGET_NANOS)
 	ICONNAME=icons/nanos_app_mina.gif
+else ifeq ($(TARGET_NAME),TARGET_STAX)
+	ICONNAME=icons/stax_app_mina.gif
 else
 	ICONNAME=icons/nanox_app_mina.gif
 endif
@@ -90,6 +92,8 @@ ifeq ($(TARGET_NAME),TARGET_NANOX)
 EMULATOR_SDK=1.2
 else ifeq ($(TARGET_NAME),TARGET_NANOS2)
 EMULATOR_SDK=1.0
+else ifeq ($(TARGET_NAME),TARGET_STAX)
+EMULATOR_SDK=1.0
 else
 EMULATOR_SDK=2.0
 endif
@@ -119,6 +123,10 @@ ifeq ($(TARGET_NAME),TARGET_NANOS)
 EMULATOR_MODEL=nanos
 endif
 
+ifeq ($(TARGET_NAME),TARGET_STAX)
+EMULATOR_MODEL=stax
+endif
+
 ifeq ("$(EMULATOR_MODEL)","")
 $(error Unknown TARGET_NAME '$(TARGET_NAME)')
 endif
@@ -137,7 +145,7 @@ endif
 
 DEFINES   += LEDGER_BUILD
 DEFINES   += OS_IO_SEPROXYHAL
-DEFINES   += HAVE_BAGL HAVE_SPRINTF
+DEFINES   += HAVE_SPRINTF
 DEFINES   += HAVE_IO_USB HAVE_L4_USBLIB IO_USB_MAX_ENDPOINTS=6 IO_HID_EP_LENGTH=64 HAVE_USB_APDU
 DEFINES   += LEDGER_MAJOR_VERSION=$(APPVERSION_M) LEDGER_MINOR_VERSION=$(APPVERSION_N) LEDGER_PATCH_VERSION=$(APPVERSION_P)
 
@@ -153,21 +161,30 @@ DEFINES       += HAVE_WEBUSB WEBUSB_URL_SIZE_B=$(shell echo -n $(WEBUSB_URL) | w
 DEFINES   += UNUSED\(x\)=\(void\)x
 DEFINES   += APPVERSION=\"$(APPVERSION)\"
 
-ifeq ($(TARGET_NAME),TARGET_NANOX)
+ifeq ($(TARGET_NAME),$(filter $(TARGET_NAME),TARGET_NANOX TARGET_STAX))
 DEFINES       += HAVE_BLE BLE_COMMAND_TIMEOUT_MS=2000
 DEFINES       += HAVE_BLE_APDU # basic ledger apdu transport over BLE
 endif
 
 ifeq ($(TARGET_NAME),TARGET_NANOS)
-DEFINES       += IO_SEPROXYHAL_BUFFER_SIZE_B=128
+    DEFINES += IO_SEPROXYHAL_BUFFER_SIZE_B=128
 else
-DEFINES       += BUILD_NANOX IO_SEPROXYHAL_BUFFER_SIZE_B=300
-DEFINES       += HAVE_GLO096
-DEFINES       += HAVE_BAGL BAGL_WIDTH=128 BAGL_HEIGHT=64
-DEFINES       += HAVE_BAGL_ELLIPSIS # long label truncation feature
-DEFINES       += HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX
-DEFINES       += HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX
-DEFINES       += HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX
+    DEFINES += IO_SEPROXYHAL_BUFFER_SIZE_B=300
+endif
+
+ifeq ($(TARGET_NAME),TARGET_STAX)
+    DEFINES += NBGL_QRCODE
+else
+    DEFINES += HAVE_BAGL HAVE_UX_FLOW
+    ifneq ($(TARGET_NAME),TARGET_NANOS)
+        DEFINES += BUILD_NANOX 
+        DEFINES += HAVE_GLO096
+        DEFINES += HAVE_BAGL BAGL_WIDTH=128 BAGL_HEIGHT=64
+        DEFINES += HAVE_BAGL_ELLIPSIS # long label truncation feature
+        DEFINES += HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX
+        DEFINES += HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX
+        DEFINES += HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX
+    endif
 endif
 
 # Both nano S and X benefit from the flow.
@@ -257,9 +274,11 @@ include $(BOLOS_SDK)/Makefile.glyphs
 ### variables processed by the common makefile.rules of the SDK to grab source files and include dirs
 APP_SOURCE_PATH  += src
 SDK_SOURCE_PATH  += lib_stusb lib_stusb_impl lib_u2f
-SDK_SOURCE_PATH  += lib_ux
+ifneq ($(TARGET_NAME),TARGET_STAX)
+SDK_SOURCE_PATH += lib_ux
+endif
 
-ifeq ($(TARGET_NAME),TARGET_NANOX)
+ifeq ($(TARGET_NAME),$(filter $(TARGET_NAME),TARGET_NANOX TARGET_STAX))
 SDK_SOURCE_PATH  += lib_blewbxx lib_blewbxx_impl
 endif
 